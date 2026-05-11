48868554be4f71d2c1227e200653a0bd4ce47102
AloofPoem <juanjito.botero@gmail.com>
2026-05-10 22:31:29 -0500

    tratando de arreglar un error de versiones

--- src/main/java/com/gestionestudiantesmedicina/entities/What.java
+++ src/main/java/com/gestionestudiantesmedicina/entities/What.java
@@ -2,4 +2,37 @@ package com.gestionestudiantesmedicina.entities;
 
 public class What {
     private String w;
+    private String hola;
+    private int sf;
+
+    public What(String w, String hola, int sf) {
+        this.w = w;
+        this.hola = hola;
+        this.sf = sf;
+    }
+
+    public String getW() {
+        return w;
+    }
+
+    public void setW(String w) {
+        this.w = w;
+    }
+
+    public String getHola() {
+        return hola;
+    }
+
+    public void setHola(String hola) {
+        this.hola = hola;
+    }
+
+    public int getSf() {
+        return sf;
+    }
+    
+    public void setSf(int sf) {
+        this.sf = sf;
+    }
+    
 }

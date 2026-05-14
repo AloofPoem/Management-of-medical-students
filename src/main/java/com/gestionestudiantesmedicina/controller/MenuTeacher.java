75119d49c99f33af84b5b1315c579bd2d0fb7455
AloofPoem <juanjito.botero@gmail.com>
2026-05-14 14:13:20 -0500

    se añadieron unos controller de menu basicos

---
+++ src/main/java/com/gestionestudiantesmedicina/controller/MenuTeacher.java
@@ -0,0 +1,42 @@
+package com.gestionestudiantesmedicina.controller;
+
+import java.io.IOException;
+
+import com.gestionestudiantesmedicina.App;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.BorderPane;
+
+public class MenuTeacher {
+     @FXML
+    private BorderPane bpMenu;
+
+    @FXML
+    private void loadStudentInfo(ActionEvent event){
+        loadView("student");
+    }
+
+    private void loadView(String fxmlName) {
+        try {
+            FXMLLoader loader = new FXMLLoader(App.class.getResource(fxmlName + ".fxml"));
+            Parent view = loader.load();
+            bpMenu.setCenter(view);
+        } catch (IOException e) {
+            e.printStackTrace();
+            showAlert("Error al Cargar", "No se pudo cargar la vista: " + fxmlName + ".fxml");
+        }
+    }
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}

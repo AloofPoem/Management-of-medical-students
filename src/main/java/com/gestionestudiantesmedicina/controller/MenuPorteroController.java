d6b955b4acee578668bfda90d1f76fd2f72e2c64
jORANGEER <galeanobarrerajuandavid@gmail.com>
2026-05-25 15:57:28 -0500

    añadi correcciones como record Portero, Menu Portero porfa revisar

---
+++ src/main/java/com/gestionestudiantesmedicina/controller/MenuPorteroController.java
@@ -0,0 +1,53 @@
+package com.gestionestudiantesmedicina.controller;
+
+import java.io.IOException;
+
+import com.gestionestudiantesmedicina.App;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.Parent;
+
+public class MenuPorteroController {
+
+    @FXML
+    private BorderPane bpMenu;
+
+    @FXML
+    private void loadRecord(ActionEvent event){
+       loadView("RecordViewPortero");
+    }
+
+     @FXML
+    void logout(ActionEvent event){
+        try {
+            App.setRoot("LoginView");
+        } catch (IOException e) {
+            showAlert(AlertType.ERROR,"Error al cerrar sesion", "No se pudo cerrar sesion");
+            e.printStackTrace();
+        }
+    }
+    private void loadView(String fxmlName) {
+        try {
+            FXMLLoader loader = new FXMLLoader(App.class.getResource(fxmlName + ".fxml"));
+            Parent view = loader.load();
+            bpMenu.setCenter(view);
+        } catch (IOException e) {
+            e.printStackTrace();
+            showAlert(AlertType.ERROR,"Error al Cargar", "No se pudo cargar la vista: " + fxmlName + ".fxml");
+        }
+    }
+
+
+    private void showAlert(AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+}
